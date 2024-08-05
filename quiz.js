936def7b7bc522af9d123109a011893a3ddc3309
adilraees786 <adilraees911@gmail.com>
2024-08-05 14:48:31 +0500

    addmcqs

---
+++ quiz.js
@@ -0,0 +1,155 @@
+const quizdb=[
+    {
+      question: "Q1:What does HTML stand for?",
+      options: ["Hyper Text Markup Language", "Hyperlinks and Text Markup Language", "Home Tool Markup Language", "Hyperlinking Text Marking Language"],
+      answer: "Hyper Text Markup Language"
+    },
+    {
+      question: "Who is making the Web standards?",
+      options: ["Mozilla", "Google", "The World Wide Web Consortium", "Microsoft"],
+      answer: "The World Wide Web Consortium"
+    },
+    {
+      question: "Choose the correct HTML element for the largest heading:",
+      options: ["<heading>", "<h1>", "<h6>", "<head>"],
+      answer: "<h1>"
+    },
+    {
+      question: "What is the correct HTML element for inserting a line break?",
+      options: ["<lb>", "<break>", "<br>", "<line>"],
+      answer: "<br>"
+    },
+    {
+      question: "What is the correct HTML for adding a background color?",
+      options: ["<body bg='yellow'>", "<body style='background-color:yellow;'>", "<background>yellow</background>", "<body background='yellow'>"],
+      answer: "<body style='background-color:yellow;'>"
+    },
+    {
+      question: "Choose the correct HTML element to define important text",
+      options: ["<b>", "<important>", "<strong>", "<i>"],
+      answer: "<strong>"
+    },
+    {
+      question: "Choose the correct HTML element to define emphasized text",
+      options: ["<italic>", "<i>", "<em>", "<strong>"],
+      answer: "<em>"
+    },
+    {
+      question: "What is the correct HTML for creating a hyperlink?",
+      options: ["<a>http://www.example.com</a>", "<a href='http://www.example.com'>Example</a>", "<a url='http://www.example.com'>Example</a>", "<a name='http://www.example.com'>Example</a>"],
+      answer: "<a href='http://www.example.com'>Example</a>"
+    },
+    {
+      question: "Which character is used to indicate an end tag?",
+      options: ["*", "/", "<", "^"],
+      answer: "/"
+    },
+    {
+      question: "How can you open a link in a new tab/browser window?",
+      options: ["<a href='url' new>", "<a href='url' target='_blank'>", "<a href='url' target='new'>", "<a href='url' open>"],
+      answer: "<a href='url' target='_blank'>"
+    },
+    {
+      question: "Which of these elements are all <table> elements?",
+      options: ["<table><head><tfoot>", "<thead><body><tr>", "<table><tr><td>", "<table><tr><tt>"],
+      answer: "<table><tr><td>"
+    },
+    {
+      question: "How can you make a numbered list?",
+      options: ["<ol>", "<ul>", "<dl>", "<list>"],
+      answer: "<ol>"
+    },
+    {
+      question: "How can you make a bulleted list?",
+      options: ["<ul>", "<ol>", "<dl>", "<list>"],
+      answer: "<ul>"
+    },
+    {
+      question: "What is the correct HTML for making a checkbox?",
+      options: ["<input type='check'>", "<check>", "<checkbox>", "<input type='checkbox'>"],
+      answer: "<input type='checkbox'>"
+    },
+    {
+      question: "What is the correct HTML for making a text input field?",
+      options: ["<input type='textfield'>", "<input type='text'>", "<textinput type='text'>", "<textfield>"],
+      answer: "<input type='text'>"
+    },
+    {
+      question: "What is the correct HTML for making a drop-down list?",
+      options: ["<input type='dropdown'>", "<select>", "<list>", "<input type='list'>"],
+      answer: "<select>"
+    },
+    {
+      question: "What is the correct HTML for making a text area?",
+      options: ["<input type='textarea'>", "<textarea>", "<textinput type='textarea'>", "<textfield>"],
+      answer: "<textarea>"
+    },
+    {
+      question: "What is the correct HTML for inserting an image?",
+      options: ["<img src='image.gif' alt='MyImage'>", "<img href='image.gif' alt='MyImage'>", "<image src='image.gif' alt='MyImage'>", "<img alt='MyImage'>image.gif</img>"],
+      answer: "<img src='image.gif' alt='MyImage'>"
+    },
+    {
+      question: "What is the correct HTML for inserting a background image?",
+      options: ["<background img='background.gif'>", "<body bg='background.gif'>", "<body style='background-image:url(background.gif)'>", "<background url='background.gif'>"],
+      answer: "<body style='background-image:url(background.gif)'>"
+    },
+    {
+      question: "An <iframe> is used to display a web page within a web page.",
+      options: ["True", "False"],
+      answer: "True"
+    },
+    {
+      question: "HTML comments start with <!-- and end with -->",
+      options: ["True", "False"],
+      answer: "True"
+    },
+    {
+      question: "Block elements are normally displayed without starting a new line.",
+      options: ["True", "False"],
+      answer: "False"
+    },
+    {
+      question: "Inline elements are normally displayed without starting a new line.",
+      options: ["True", "False"],
+      answer: "True"
+    },
+    {
+      question: "Which HTML attribute is used to define inline styles?",
+      options: ["class", "style", "font", "styles"],
+      answer: "style"
+    },
+    {
+      question: "Which HTML attribute is used to define CSS styles?",
+      options: ["class", "styles", "style", "font"],
+      answer: "class"
+    },
+    {
+      question: "Which is the correct CSS syntax?",
+      options: ["{body;color:black;}", "body:color=black;", "{body:color=black;}", "body {color: black;}"],
+      answer: "body {color: black;}"
+    },
+    {
+      question: "How do you display hyperlinks without an underline?",
+      options: ["a {text-decoration:none;}", "a {decoration:no-underline;}", "a {underline:none;}", "a {text-decoration:no-underline;}"],
+      answer: "a {text-decoration:none;}"
+    },
+    {
+      question: "Which property is used to change the background color?",
+      options: ["bgcolor", "color", "background-color", "bg-color"],
+      answer: "background-color"
+    },
+    {
+      question: "Which property is used to change the text color of an element?",
+      options: ["fgcolor", "text-color", "color", "font-color"],
+      answer: "color"
+    },
+    {
+      question: "Which HTML element is used to specify a footer for a document or section?",
+      options: ["<bottom>", "<footer>", "<section>", "<foot>"],
+      answer: "<footer>"
+    }
+  ]
+    
+    
+     
